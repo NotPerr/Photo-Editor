c23b8bff54fa3ebd8bb2a71c8b63d02ac1860cd6
NotPerr <vovo7292@gmail.com>
2023-04-20 14:48:12 +0800

    add rotate and flip function

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ fileOptions = document.querySelectorAll(".filter button"),
 filterName = document.querySelector(".filter-info .name"),
 filterValue = document.querySelector(".filter-info .value"),
 filterSlider = document.querySelector(".slider input"),
+rotateOptions = document.querySelectorAll(".rotate button"),
 previewImg = document.querySelector(".preview-img img"),
 chooseImgBtn = document.querySelector(".choose-img");
 
 let brightness = 100, saturation = 100, inversion = 0, grayscale = 0;
+let rotate = 0, flipHorizontal = 1, flipVertical = 1;
 
 const applyFilters = () => {
+    previewImg.style.transform = `rotate(${rotate}deg) scale(${flipHorizontal},${flipVertical})`;
     previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`
 }
 
@@ -65,6 +68,21 @@ const updateFilter = () => {
 
     applyFilters();
 }
+
+rotateOptions.forEach(option => {
+    option.addEventListener("click",() => {
+        if(option.id === "left") {
+            rotate -= 90;
+        }else if(option.id === "right") {
+            rotate += 90;
+        }else if(option.id === "horizontal") {
+            flipHorizontal = flipHorizontal === 1? -1 : 1;
+        }else {
+            flipVertical = flipVertical === 1? -1 : 1;
+        }
+        applyFilters();
+    })
+})
 
 fileInput.addEventListener("change",loadImage);
 filterSlider.addEventListener("input",updateFilter);
